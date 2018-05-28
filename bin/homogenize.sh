b1bf18f8e9f4959a575af4f7f6203bc40b109f2c
Tangui Morlier <tangui@tangui.eu.org>
2018-05-28 18:37:40 +0200

    Conservation du bug corrigé par bfea9cbc90591a54637c5b2e79e2f231b6f9bf0c + rollback d'un effet de bord

--- bin/homogenize.sh
+++ bin/homogenize.sh
@@ -2,6 +2,6 @@
 
 md=$1
 
-cat $md | sed 's/\*\**//g' | sed 's/[\*^]//g' | sed 's/_//g' | sed 's/\\//g'  | sed 's/^[ \.] *//'  | sed 's/^\(Artfide\|Artcile\) /Article /' | sed 's/Art \. /Article /' | sed 's/ \(1\|premi\|I\)er-*/ 1/' | sed 's/Art[^i] */Article /i' | sed 's/^Articles /Article /' | sed 's/^Article\.* \([0-9][0-9\-]*\) *[\.\-\—]* */Article \1\n\n/i' | tr '\n' '|' | sed 's/\([0-9]\)||*\(bis\|ter\)\.*/\1 \2||/gi' | sed 's/|||*/\n\n/g'  | sed 's/|/ /g'  | sed 's/^[—  ]*//' | sed 's/^Article/# Article/' > $md"."$$
+cat $md | sed 's/\*\**//g' | sed 's/[\*^]//g' | sed 's/_//g' | sed 's/\\//g'  | sed 's/^[ \.] *//'  | sed 's/^\(Artfide\|Artcile\) /Article /' | sed 's/^Art \. /Article /' | sed 's/ \(1\|premi\|I\)er-*/ 1/' | sed 's/^Art[^i] */Article /i' | sed 's/^Articles /Article /' | sed 's/^Article\.* \([0-9][0-9\-]*\) *[\.\-\—]* */Article \1\n\n/i' | tr '\n' '|' | sed 's/\([0-9]\)||*\(bis\|ter\)\.*/\1 \2||/gi' | sed 's/|||*/\n\n/g'  | sed 's/|/ /g'  | sed 's/^[—  ]*//' | sed 's/^Article/# Article/' > $md"."$$
 mv $md"."$$ $md
 
